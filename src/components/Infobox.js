f5bba8f3ce26be0ebb78f88319287c06249557b1
Benjamin <benman2005@gMAIL.com>
2019-01-23 19:03:26 +0100

    removed react-fittext

--- src/components/Infobox.js
+++ src/components/Infobox.js
@@ -1,10 +1,6 @@
 import React, { PureComponent } from "react";
-// import ReactFitText from "react-fittext";
 
 class InfoBox extends PureComponent {
-  componentDidMount(){
-    console.log(document.getElementById('infoBox').clientWidth)
-  }
   render() {
     const style = {
       backgroundColor: "rgb(230, 235, 244)",
@@ -14,7 +10,7 @@ class InfoBox extends PureComponent {
       width: "150px",
       marginTop: "102%",
       marginLeft: "100%",
-      overflowWrap: "break-word",
+      overflowWrap: "break-word"
     };
     const paragraphStyle = {
       margin: "1px 10px 1px 10px",
@@ -22,14 +18,15 @@ class InfoBox extends PureComponent {
       fontFamily: "'Montserrat', sans-serif"
     };
     return (
-      <div id="infoBox"style={style}>
-        {/* <ReactFitText> */}
-          <p style={paragraphStyle}>{this.props.details.name}</p>
-        {/* </ReactFitText> */}
+      <div id="infoBox" style={style}>
+        <p style={paragraphStyle}>{this.props.details.name}</p>
         {this.props.details.location.distance && (
-            <p style={paragraphStyle}> ({this.props.details.location.distance}m)</p>
+          <p style={paragraphStyle}>
+            {" "}
+            ({this.props.details.location.distance}m)
+          </p>
         )}
-          <p style={paragraphStyle}>{this.props.details.categories[0].name}</p>
+        <p style={paragraphStyle}>{this.props.details.categories[0].name}</p>
       </div>
     );
   }
